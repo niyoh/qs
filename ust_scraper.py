205feb7ca2ba2ec2db9d1a2067fa0913e7d00585
martin <martin@martin.martin>
2023-11-05 18:10:16 +0800

    use json api instead

--- ust_scraper.py
+++ ust_scraper.py
@@ -1,107 +1,108 @@
 import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import requests
 import sqlite3
 from datetime import datetime
 
 
 def ust_scraper():
-    driver = webdriver.Chrome()
-    driver.get('https://treasurydirect.gov/auctions/upcoming/')
-
     writetime = datetime.utcnow()
 
     conn = sqlite3.connect('ust.db')
     create_table_query = '''
         CREATE TABLE IF NOT EXISTS auctioned (
-            "Version" TIMESTAMP,
-            "Product" TEXT,
-            "Term" TEXT,
-            "CMB" TEXT,
-            "Reopening" TEXT,
-            "CUSIP" TEXT,
-            "Issue Date" TEXT,
-            "High Rate" TEXT,
-            "High Yield" TEXT,
-            "High Discount Margin" TEXT,
-            "Investment Rate" TEXT,
-            "Interest Rate" TEXT,
-            "Price per $100" REAL
+            "version" TIMESTAMP,
+            "type" TEXT,
+            "term" TEXT,
+            "reopening" TEXT,
+            "cusip" TEXT,
+            "issueDate" TIMESTAMP,
+            "highYield" REAL,
+            "highDiscountMargin" REAL,
+            "interestRate" REAL,
+            "highPrice" REAL,
+            "blob" TEXT
         );
     '''
+    auc_table_cols = ['version', 'type', 'term', 'reopening', 'cusip', 'issueDate', 'highYield', 'highDiscountMargin',
+                      'interestRate', 'highPrice', 'blob']
     conn.execute(create_table_query)
     create_table_query = '''
         CREATE TABLE IF NOT EXISTS upcoming (
-            "Version" TIMESTAMP,
-            "Product" TEXT,
-            "Term" TEXT,
-            "CMB" TEXT,
-            "Reopening" TEXT,
-            "CUSIP" TEXT,
-            "Offering Amount", TEXT,
-            "Announcement Date" TIMESTAMP,
-            "Auction Date" TIMESTAMP,
-            "Issue Date" TIMESTAMP
+            "version" TIMESTAMP,
+            "type" TEXT,
+            "term" TEXT,
+            "reopening" TEXT,
+            "cusip" TEXT,
+            "offeringAmount", TEXT,
+            "announcementDate" TIMESTAMP,
+            "auctionDate" TIMESTAMP,
+            "issueDate" TIMESTAMP,
+            "blob" TEXT
         );
     '''
-    # PRIMARY KEY ("Version", "Product", "Term")
+    upc_table_cols = ['version', 'type', 'term', 'reopening', 'cusip', 'offeringAmount', 'announcementDate',
+                      'auctionDate', 'issueDate', 'blob']
     conn.execute(create_table_query)
 
-    wait = WebDriverWait(driver, 10)
+    types = ['Bill', 'Note', 'Bond', 'TIPS', 'FRN']
 
     auc_tables = []
-    for id in ['institTableBills', 'institTableNotes', 'institTableBonds', 'institTableTIPS', 'institTableFRN']:
-        t = scrape_table(wait, id)
+    for p in types:
+        t = scrape_auctioned(p)
         auc_tables.append(process(t, writetime))
 
     upc_tables = []
-    for id in ['institTableBillsUpcoming', 'institTableNotesUpcoming', 'institTableBondsUpcoming',
-               'institTableTIPSUpcoming', 'institTableFRNUpcoming']:
-        t = scrape_table(wait, id)
+    for p in types:
+        t = scrape_upcoming(p)
         upc_tables.append(process(t, writetime))
 
     for t in auc_tables:
-        t.to_sql('auctioned', conn, if_exists='append', index=False)
+        t[auc_table_cols].to_sql('auctioned', conn, if_exists='append', index=False)
     for t in upc_tables:
-        t.to_sql('upcoming', conn, if_exists='append', index=False)
+        t[upc_table_cols].to_sql('upcoming', conn, if_exists='append', index=False)
 
     conn.close()
 
-    sec_detail = 'https://treasurydirect.gov/auctions/auction-query/?cusip='
 
-    # Close the browser
-    driver.quit()
+def sec_details_url(cusip):
+    return f'https://www.treasurydirect.gov/TA_WS/securities/jqsearch?format=json&cusipoperator=and&filtervalue0={cusip}&filtercondition0=CONTAINS&filteroperator0=0&filterdatafield0=cusip&filterGroups%5B0%5D%5Bfield%5D=cusip&filterGroups%5B0%5D%5Bfilters%5D%5B0%5D%5Blabel%5D=912797GM3&filterGroups%5B0%5D%5Bfilters%5D%5B0%5D%5Bvalue%5D=912797GM3&filterGroups%5B0%5D%5Bfilters%5D%5B0%5D%5Bcondition%5D=CONTAINS&filterGroups%5B0%5D%5Bfilters%5D%5B0%5D%5Boperator%5D=and&filterGroups%5B0%5D%5Bfilters%5D%5B0%5D%5Bfield%5D=cusip&filterGroups%5B0%5D%5Bfilters%5D%5B0%5D%5Btype%5D=stringfilter&filterscount=1&groupscount=0&pagenum=0&pagesize=100&recordstartindex=0&recordendindex=100'
+
+
+def auctioned_url(type):
+    return f'https://www.treasurydirect.gov/TA_WS/securities/auctioned?format=json&limitByTerm=true&type={type}&days=720'
+
+
+def upcoming_url(type):
+    return f'https://www.treasurydirect.gov/TA_WS/securities/upcoming?format=json&limitByTerm=true&type={type}'
 
 
 def process(instrument, writetime):
-    if not instrument['Term'].str.match(r'.+-Week|.+-Day|.+-Year').all():
-        raise ValueError(f"The string '{instrument[product]}' does not match the expected format.")
-    if not instrument['CUSIP'].str.match(r'^[A-Za-z0-9]{9}$').all():
+    if not instrument['term'].str.match(r'.+-Week|.+-Day|.+-Year').all():
+        raise ValueError(f"The string '{instrument[type]}' does not match the expected format.")
+    if not instrument['cusip'].str.match(r'^[A-Za-z0-9]{9}$').all():
         raise ValueError(f"The string '{instrument['CUSIP']}' does not match the expected format.")
-    instrument['Version'] = writetime
+    instrument['version'] = writetime
     return instrument
 
 
-def scrape_table(driver, id):
-    table = driver.until(EC.presence_of_element_located((By.ID, id)))
+def scrape_auctioned(type):
+    response = requests.request(method='GET', url=auctioned_url(type))
+    if response.status_code != 200:
+        raise Exception(f'unable to scrape auctioned type: {type}')
 
-    # Extract the table headers / rows
-    headers = [th.get_attribute('innerText') for th in table.find_elements(By.TAG_NAME, 'th')]
-    product = headers[0]
-    headers[0] = 'Term'
-    rows = []
-    for row in table.find_elements(By.TAG_NAME, 'tr'):
-        data = [td.get_attribute('innerText') for td in row.find_elements(By.TAG_NAME, 'td')]
-        if len(data) == len(headers):
-            rows.append(data)
+    auc = pd.read_json(response.text)
+    auc['blob'] = response.text
+    return auc
 
-    table = pd.DataFrame(rows[1:], columns=headers)
-    table['Product'] = product
-    if 'Price per $100' in table:
-        table['Price per $100'] = table['Price per $100'].str.replace('[$,]', '')
-    return table
+
+def scrape_upcoming(type):
+    response = requests.request(method='GET', url=auctioned_url(type))
+    if response.status_code != 200:
+        raise Exception(f'unable to scrape upcoming type: {type}')
+
+    upc = pd.read_json(response.text)
+    upc['blob'] = response.text
+    return upc
 
 
 if __name__ == '__main__':
