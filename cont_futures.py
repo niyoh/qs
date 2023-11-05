9814124028edafcf17da2a6a9ba53814ee24241b
martin <martin@martin.martin>
2023-11-05 20:21:20 +0800

    refactor

--- cont_futures.py
+++ cont_futures.py
@@ -7,129 +7,136 @@ logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger()
 
 
-def near_exp_continuous_futures(full_ref_trd: pd.DataFrame, code: str, n: int):
-    ref_trd = full_ref_trd.query(f"fut_code == '{code}'")
-    if len(ref_trd) == 0:
-        logger.warning(f'no trade & refs for future: {code}')
+class ContinuousFuturesProcessor:
+    def __init__(self, full_ref_trd):
+        self.full_ref_trd = full_ref_trd
 
-    ref_trd['delist_mth'] = ref_trd['delist_date'].dt.month
+    def near_exp_continuous_futures(self, code: str, n: int):
+        ref_trd = self.full_ref_trd.query(f"fut_code == '{code}'").copy()
+        if len(ref_trd) == 0:
+            logger.warning(f'no trade & refs for future: {code}')
 
-    # rank c1
-    c1 = ref_trd.groupby('trade_date').apply(lambda x: x.loc[x['delist_date'].idxmin()])
+        ref_trd['delist_mth'] = ref_trd['delist_date'].dt.month
 
-    # (exclude outliers when delist_date is too far away from trade_date, happening at initial portion)
-    trd_c1_gap = pd.DataFrame(c1['delist_date'] - c1['trade_date'], columns=['gap'])
-    trd_c1_gap_m = trd_c1_gap['gap'].mean()
-    trd_c1_gap_sd = trd_c1_gap['gap'].std()
-    trd_c1_gap['zscore'] = trd_c1_gap.apply(lambda x: abs(x - trd_c1_gap_m) / trd_c1_gap_sd)
-    c1 = c1[trd_c1_gap['zscore'] <= 1.5]
+        # rank c1
+        c1 = ref_trd.groupby('trade_date').apply(lambda x: x.loc[x['delist_date'].idxmin()])
 
-    # update c1 label in main ref/trades
-    c1['series'] = 'c1'
-    c1['c1_mth'] = c1['delist_date'].dt.month
-    ref_trd = ref_trd[ref_trd['trade_date'].isin(c1.index)]
-    ref_trd = pd.merge(ref_trd, c1[['ts_code', 'series', 'c1_mth']].reset_index(),
-                       on=['trade_date', 'ts_code'],
-                       how='left')
-    ref_trd.sort_values(['trade_date', 'delist_date'], inplace=True)
-    ref_trd['c1_mth'].ffill(inplace=True)
-    ref_trd['c1_plus_mth'] = (ref_trd['delist_mth'] - ref_trd['c1_mth']) % 12
+        # (exclude outliers when delist_date is too far away from trade_date, happening at initial portion)
+        trd_c1_gap = pd.DataFrame(c1['delist_date'] - c1['trade_date'], columns=['gap'])
+        trd_c1_gap_m = trd_c1_gap['gap'].mean()
+        trd_c1_gap_sd = trd_c1_gap['gap'].std()
+        trd_c1_gap['zscore'] = trd_c1_gap.apply(lambda x: abs(x - trd_c1_gap_m) / trd_c1_gap_sd)
+        c1 = c1[trd_c1_gap['zscore'] <= 1.5]
 
-    # rank c2,c3 - assuming c2~=c1+1mth, c3~=c1+2mth
-    for i in range(1, n):
-        ref_trd.loc[ref_trd['c1_plus_mth'] == i, 'series'] = f'c{i + 1}'
-    ref_trd = ref_trd[~ref_trd['series'].isna()]
-
-    # tables: 1) trade_date -> px  2) trade_date -> ref
-    px = ref_trd.pivot(index='trade_date', columns='series', values='close')
-    ref = ref_trd.pivot(index='trade_date', columns='series', values='ts_code')
-
-    # forward fill price for gaps in c2,c3
-    px.columns.map(lambda x: px[x].ffill(inplace=True))
-
-    px_adj, roll_dates = hist_adj(ref, px)
-    return px, px_adj, roll_dates
-
-
-def most_active_continuous_futures(full_ref_trd: pd.DataFrame, code: str, n: int):
-    ref_trd = full_ref_trd.query(f"fut_code == '{code}'")
-    if len(ref_trd) == 0:
-        logger.warning(f'no trade & refs for future: {code}')
-
-    ref_trd['delist_mth'] = ref_trd['delist_date'].dt.month
-
-    # rank
-    ref_trd['series'] = np.nan
-    ref_trd['last_delist_date'] = pd.Timestamp.min
-    for i in range(0, n):
-        # filter futures with unknown series
-        # filter futures with >= delist_date then last series
-        ref_trd_i = ref_trd[pd.isna(ref_trd['series'])]
-        ref_trd_i = ref_trd_i[ref_trd_i['delist_date'] >= ref_trd_i['last_delist_date']]
-
-        # rank by largest open-interest
-        ref_trd_i = ref_trd_i.groupby('trade_date').apply(lambda x: x.loc[x['oi'].idxmax()])
-
-        ref_trd_i['series'] = f'v{i + 1}'
-        ref_trd_i['last_delist_date'] = ref_trd_i['delist_date']
-
-        # update new series label in main ref/trades
-        series = ref_trd[['series']]
-        ref_trd.drop('series', axis=1, inplace=True)
-        if 'last_delist_date' in ref_trd: ref_trd.drop('last_delist_date', axis=1, inplace=True)
-        ref_trd = pd.merge(ref_trd, ref_trd_i[['ts_code', 'series', 'last_delist_date']].reset_index(),
+        # update c1 label in main ref/trades
+        c1['series'] = 'c1'
+        c1['c1_mth'] = c1['delist_date'].dt.month
+        ref_trd = ref_trd[ref_trd['trade_date'].isin(c1.index)]
+        ref_trd = pd.merge(ref_trd, c1[['ts_code', 'series', 'c1_mth']].reset_index(),
                            on=['trade_date', 'ts_code'],
                            how='left')
+        ref_trd.sort_values(['trade_date', 'delist_date'], inplace=True)
+        ref_trd['c1_mth'].ffill(inplace=True)
+        ref_trd['c1_plus_mth'] = (ref_trd['delist_mth'] - ref_trd['c1_mth']) % 12
 
-        ref_trd.loc[series.dropna().index, 'series'] = series.dropna()
+        # rank c2,c3 - assuming c2~=c1+1mth, c3~=c1+2mth
+        for i in range(1, n):
+            ref_trd.loc[ref_trd['c1_plus_mth'] == i, 'series'] = f'c{i + 1}'
+        ref_trd = ref_trd[~ref_trd['series'].isna()]
 
-        ref_trd['last_delist_date'].ffill(inplace=True)
-        print(ref_trd_i)
+        # tables: 1) trade_date -> px  2) trade_date -> ref
+        px = ref_trd.pivot(index='trade_date', columns='series', values='close')
+        ref = ref_trd.pivot(index='trade_date', columns='series', values='ts_code')
 
-    # cleanup irrelevant futures and sort series
-    ref_trd.sort_values(['trade_date', 'series'], inplace=True)
-    ref_trd = ref_trd[~ref_trd['series'].isna()]
-    ref_trd.drop('last_delist_date', axis=1, inplace=True)
+        # forward fill price for gaps in c2,c3
+        px.columns.map(lambda x: px[x].ffill(inplace=True))
 
-    # tables: 1) trade_date -> px  2) trade_date -> ref
-    px = ref_trd.pivot(index='trade_date', columns='series', values='close')
-    ref = ref_trd.pivot(index='trade_date', columns='series', values='ts_code')
+        px_adj, roll_dates = self.hist_adj(ref, px)
+        return px, px_adj, roll_dates
 
-    # forward fill price for gaps in c2,c3
-    px.columns.map(lambda x: px[x].ffill(inplace=True))
+    def most_active_continuous_futures(self, code: str, n: int):
+        ref_trd = self.full_ref_trd.query(f"fut_code == '{code}'").copy()
+        if len(ref_trd) == 0:
+            logger.warning(f'no trade & refs for future: {code}')
 
-    px_adj, roll_dates = hist_adj(ref, px)
-    return px, px_adj, roll_dates
+        ref_trd['delist_mth'] = ref_trd['delist_date'].dt.month
 
+        # rank
+        ref_trd['series'] = np.nan
+        ref_trd['last_delist_date'] = pd.Timestamp.min
+        for i in range(0, n):
+            # filter futures with unknown series
+            # filter futures with >= delist_date then last series
+            ref_trd_i = ref_trd[pd.isna(ref_trd['series'])]
+            ref_trd_i = ref_trd_i[ref_trd_i['delist_date'] >= ref_trd_i['last_delist_date']]
 
-def preprocess(ref: pd.DataFrame, trd: pd.DataFrame):
-    ref_trd = pd.merge(ref, trd, on='ts_code').sort_values(by='trade_date')
-    return ref_trd
+            # rank by largest open-interest
+            ref_trd_i = ref_trd_i.groupby('trade_date').apply(lambda x: x.loc[x['oi'].idxmax()])
 
+            ref_trd_i['series'] = f'v{i + 1}'
+            ref_trd_i['last_delist_date'] = ref_trd_i['delist_date']
 
-def hist_adj(ref, px):
-    px_adj = px.copy()
-    roll_dates = {}
-    for i in range(0, len(px.columns)):
-        n = px.columns[i]
+            # update new series label in main ref/trades
+            series = ref_trd[['series']]
+            ref_trd.drop('series', axis=1, inplace=True)
+            if 'last_delist_date' in ref_trd: ref_trd.drop('last_delist_date', axis=1, inplace=True)
+            ref_trd = pd.merge(ref_trd, ref_trd_i[['ts_code', 'series', 'last_delist_date']].reset_index(),
+                               on=['trade_date', 'ts_code'],
+                               how='left')
 
-        px_n = px[[n]].copy()
-        px_n['last'] = px_n[n].shift(1)
+            ref_trd.loc[series.dropna().index, 'series'] = series.dropna()
 
-        # enumerate last close date before roll
-        ref_n = ref[[n]].copy()
-        ref_n[n].ffill(inplace=True)
-        roll_dates[n] = ref_n[n] != ref_n[n].shift(1)
-        roll_closes_n = roll_dates[n].shift(-1).fillna(False)
+            ref_trd['last_delist_date'].ffill(inplace=True)
+            print(ref_trd_i)
 
-        # calc multiplier before each roll, cumulatively adjust for historical dates
-        roll_mult = px_n[roll_closes_n][n] / px_n[roll_closes_n]['last']
-        mult = roll_mult[::-1].cumprod()[::-1]
-        mult = mult.reindex(px.index).bfill().fillna(1)
+        # cleanup irrelevant futures and sort series
+        ref_trd.sort_values(['trade_date', 'series'], inplace=True)
+        ref_trd = ref_trd[~ref_trd['series'].isna()]
+        ref_trd.drop('last_delist_date', axis=1, inplace=True)
 
-        # adjust
-        px_adj[n] = px[n] * mult
-    return px_adj, roll_dates
+        # tables: 1) trade_date -> px  2) trade_date -> ref
+        px = ref_trd.pivot(index='trade_date', columns='series', values='close')
+        ref = ref_trd.pivot(index='trade_date', columns='series', values='ts_code')
+
+        # forward fill price for gaps in c2,c3
+        px.columns.map(lambda x: px[x].ffill(inplace=True))
+
+        px_adj, roll_dates = self.hist_adj(ref, px)
+        return px, px_adj, roll_dates
+
+    @staticmethod
+    def preprocess(ref: pd.DataFrame, trd: pd.DataFrame):
+        trd['trade_date'] = pd.to_datetime(trd['trade_date'], format='%Y%m%d')
+        ref['list_date'] = pd.to_datetime(ref['list_date'], format='%Y%m%d')
+        ref['delist_date'] = pd.to_datetime(ref['delist_date'], format='%Y%m%d')
+
+        ref_trd = pd.merge(ref, trd, on='ts_code').sort_values(by='trade_date')
+        return ref_trd
+
+    @staticmethod
+    def hist_adj(ref, px):
+        px_adj = px.copy()
+        roll_dates = {}
+        for i in range(0, len(px.columns)):
+            n = px.columns[i]
+
+            px_n = px[[n]].copy()
+            px_n['last'] = px_n[n].shift(1)
+
+            # enumerate last close date before roll
+            ref_n = ref[[n]].copy()
+            ref_n[n].ffill(inplace=True)
+            roll_dates[n] = ref_n[n] != ref_n[n].shift(1)
+            roll_closes_n = roll_dates[n].shift(-1).fillna(False)
+
+            # calc multiplier before each roll, cumulatively adjust for historical dates
+            roll_mult = px_n[roll_closes_n][n] / px_n[roll_closes_n]['last']
+            mult = roll_mult[::-1].cumprod()[::-1]
+            mult = mult.reindex(px.index).bfill().fillna(1)
+
+            # adjust
+            px_adj[n] = px[n] * mult
+        return px_adj, roll_dates
 
 
 def visualize(prefix, px, px_adj, roll_dates):
@@ -152,14 +159,10 @@ if __name__ == '__main__':
     fut_ref = pd.read_csv(future_ref_file_path)
     fut_trd = pd.read_csv(future_price_file_path)
 
-    fut_trd['trade_date'] = pd.to_datetime(fut_trd['trade_date'], format='%Y%m%d')
-    fut_ref['list_date'] = pd.to_datetime(fut_ref['list_date'], format='%Y%m%d')
-    fut_ref['delist_date'] = pd.to_datetime(fut_ref['delist_date'], format='%Y%m%d')
+    processor = ContinuousFuturesProcessor(ContinuousFuturesProcessor.preprocess(fut_ref, fut_trd))
 
-    ref_trd = preprocess(fut_ref, fut_trd)
-
-    if_px, if_px_adj, if_roll_dates = near_exp_continuous_futures(ref_trd, 'IF', n=3)
-    p_px, p_px_adj, p_roll_dates = most_active_continuous_futures(ref_trd, 'P', n=3)
+    if_px, if_px_adj, if_roll_dates = processor.near_exp_continuous_futures('IF', n=3)
+    p_px, p_px_adj, p_roll_dates = processor.most_active_continuous_futures('P', n=3)
 
     visualize('IF', if_px, if_px_adj, if_roll_dates)
     visualize('P', p_px, p_px_adj, p_roll_dates)
